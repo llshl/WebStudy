dc7b45514bcdceb74cb871ca11cfb24262434680
llshl <52540882+llshl@users.noreply.github.com>
2021-03-11 15:50:01 +0900

    Delete temp directory

--- temp/domain/DeliveryStatus.java
+++
@@ -1,4 +0,0 @@
-package com.example.BootStrap_AdminTest.domain;
-
-public enum DeliveryStatus {
-}
--- temp/domain/OrderStatus.java
+++
@@ -1,5 +0,0 @@
-package com.example.BootStrap_AdminTest.domain;
-
-public enum OrderStatus {
-    ORDER, CANCEL
-}
